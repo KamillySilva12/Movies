5b64a3695abd4c55bf5f8c71529f290cf8c43246
kamillysilva12 <kaamillysilvasantos@gmail.com>
2026-02-20 21:36:24 -0300

    splash

--- app/src/main/java/br/com/etecia/movies/RecuperarActivity.java
+++ app/src/main/java/br/com/etecia/movies/RecuperarActivity.java
@@ -8,18 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashActivity extends AppCompatActivity {
+public class RecuperarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.splash_layout);
+        setContentView(R.layout.recuperar_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }
